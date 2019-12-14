2b3fd6bfef7008b2b46573043623aa1544a74ade
Mitrasish Mukherjee <mmitrasish97@gmail.com>
2019-12-15 04:18:49 +0530

    build added

---
+++ build/precache-manifest.9f85c50854f373c9d6025c90c72d2857.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6b9f7fa4781e2ba44b961f7406573c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49f2c31f0157f360c279",
+    "url": "/static/css/2.d2e2750e.chunk.css"
+  },
+  {
+    "revision": "b0520dfc5b3a9be434fc",
+    "url": "/static/css/main.9fca9343.chunk.css"
+  },
+  {
+    "revision": "49f2c31f0157f360c279",
+    "url": "/static/js/2.c79e9e21.chunk.js"
+  },
+  {
+    "revision": "b0520dfc5b3a9be434fc",
+    "url": "/static/js/main.4d39d151.chunk.js"
+  },
+  {
+    "revision": "2f0346e29800a26514d2",
+    "url": "/static/js/runtime-main.7818bb7c.js"
+  }
+]);
